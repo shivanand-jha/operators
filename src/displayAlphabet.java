84a7beba0d90e1bcecddcb75827c865082311e03
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-13 22:19:33 +0530

    display Alphabet

---
+++ src/displayAlphabet.java
@@ -0,0 +1,10 @@
+public class displayAlphabet {
+    public static void main(String[] args) {
+        char c = 'A';
+        for (int i = 0; i < 26; i++) {
+
+            System.out.print(c+" ");
+            c++;
+        }
+    }
+}
