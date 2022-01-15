0accc02db608d635ed1768a9279a574777e0bedc
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-01-15 14:11:36 +0530

    AP Series

--- src/APSeries.java
+++ src/APSeries.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class APSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the starting point of the number for AP");
+        int a =sc.nextInt();
         System.out.println("enter the number till AP get printed ");
         int n= sc.nextInt();
         System.out.println("enter the basic difference for AP");
         int d= sc.nextInt();
-        for (int i = 1; i < n; i=i+d) {
+        for (int i = a; i < n; i=i+d) {
             System.out.println(i);
         }
     }
